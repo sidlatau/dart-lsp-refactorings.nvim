4bc1477ed9e9561d314620966120a43a40126dc7
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-11-20 17:55:37 +0200

    refact: use only dartls server

--- lua/dart-lsp-refactorings/init.lua
+++ lua/dart-lsp-refactorings/init.lua
@@ -28,19 +28,12 @@ function M.rename(new_name, options)
 	local bufnr = options.bufnr or api.nvim_get_current_buf()
 	local clients = vim.lsp.get_active_clients({
 		bufnr = bufnr,
-		name = options.name,
+		name = "dartls",
 	})
-	if options.filter then
-		clients = vim.tbl_filter(options.filter, clients)
-	end
-
-	-- Clients must at least support rename, prepareRename is optional
-	clients = vim.tbl_filter(function(client)
-		return client.supports_method("textDocument/rename")
-	end, clients)
-
-	if #clients == 0 then
-		vim.notify("[LSP] Rename, no matching language servers with rename capability.")
+	local client = clients[1]
+	if not client then
+		vim.lsp.rename(new_name, options)
+		return
 	end
 
 	local win = api.nvim_get_current_win()
@@ -63,82 +56,44 @@ function M.rename(new_name, options)
 		)[1]
 	end
 
-	local try_use_client
-	try_use_client = function(idx, client)
-		if not client then
-			return
-		end
-
-		---@private
-		local function rename(name, will_rename_files_result)
-			local params = util.make_position_params(win, client.offset_encoding)
-			params.newName = name
-			local handler = client.handlers["textDocument/rename"] or vim.lsp.handlers["textDocument/rename"]
-			client.request("textDocument/rename", params, function(...)
-				handler(...)
-				try_use_client(next(clients, idx))
-				if will_rename_files_result then
-					-- the `will_rename_files_result` contains all the places we need to update imports
-					-- so we apply those edits.
-					vim.lsp.util.apply_workspace_edit(will_rename_files_result, client.offset_encoding)
-				end
-			end, bufnr)
-		end
-
-		local function rename_fix_imports(name)
-			if is_file_rename then
-				local old_file_path = vim.fn.expand("%:p")
-				local new_filename = refactoring_utils.file_name_for_class_name(name)
-				local actual_file_head = vim.fn.expand("%:p:h")
-				local new_file_path = refactoring_utils.path_join(actual_file_head, new_filename)
-				will_rename_files(old_file_path, new_file_path, function(result)
-					rename(name, result)
-				end)
-			else
-				rename(name)
+	---@private
+	local function rename(name, will_rename_files_result)
+		local params = util.make_position_params(win, client.offset_encoding)
+		params.newName = name
+		local handler = client.handlers["textDocument/rename"] or vim.lsp.handlers["textDocument/rename"]
+		client.request("textDocument/rename", params, function(...)
+			handler(...)
+			if will_rename_files_result then
+				-- the `will_rename_files_result` contains all the places we need to update imports
+				-- so we apply those edits.
+				vim.lsp.util.apply_workspace_edit(will_rename_files_result, client.offset_encoding)
 			end
-		end
+		end, bufnr)
+	end
 
-		if client.supports_method("textDocument/prepareRename") then
-			local params = util.make_position_params(win, client.offset_encoding)
-			client.request("textDocument/prepareRename", params, function(err, result)
-				if err or result == nil then
-					if next(clients, idx) then
-						try_use_client(next(clients, idx))
-					else
-						local msg = err and ("Error on prepareRename: " .. (err.message or "")) or "Nothing to rename"
-						vim.notify(msg, vim.log.levels.INFO)
-					end
-					return
-				end
-
-				if new_name then
-					rename_fix_imports(new_name)
-					return
-				end
-
-				local prompt_opts = {
-					prompt = "New Name: ",
-				}
-				-- result: Range | { range: Range, placeholder: string }
-				if result.placeholder then
-					prompt_opts.default = result.placeholder
-				elseif result.start then
-					prompt_opts.default = get_text_at_range(result, client.offset_encoding)
-				elseif result.range then
-					prompt_opts.default = get_text_at_range(result.range, client.offset_encoding)
-				else
-					prompt_opts.default = cword
-				end
-				vim.ui.input(prompt_opts, function(input)
-					if not input or #input == 0 then
-						return
-					end
-					rename_fix_imports(input)
-				end)
-			end, bufnr)
+	local function rename_fix_imports(name)
+		if is_file_rename then
+			local old_file_path = vim.fn.expand("%:p")
+			local new_filename = refactoring_utils.file_name_for_class_name(name)
+			local actual_file_head = vim.fn.expand("%:p:h")
+			local new_file_path = refactoring_utils.path_join(actual_file_head, new_filename)
+			will_rename_files(old_file_path, new_file_path, function(result)
+				rename(name, result)
+			end)
 		else
-			assert(client.supports_method("textDocument/rename"), "Client must support textDocument/rename")
+			rename(name)
+		end
+	end
+
+	if client.supports_method("textDocument/prepareRename") then
+		local params = util.make_position_params(win, client.offset_encoding)
+		client.request("textDocument/prepareRename", params, function(err, result)
+			if err or result == nil then
+				local msg = err and ("Error on prepareRename: " .. (err.message or "")) or "Nothing to rename"
+				vim.notify(msg, vim.log.levels.INFO)
+				return
+			end
+
 			if new_name then
 				rename_fix_imports(new_name)
 				return
@@ -146,18 +101,42 @@ function M.rename(new_name, options)
 
 			local prompt_opts = {
 				prompt = "New Name: ",
-				default = cword,
 			}
+			-- result: Range | { range: Range, placeholder: string }
+			if result.placeholder then
+				prompt_opts.default = result.placeholder
+			elseif result.start then
+				prompt_opts.default = get_text_at_range(result, client.offset_encoding)
+			elseif result.range then
+				prompt_opts.default = get_text_at_range(result.range, client.offset_encoding)
+			else
+				prompt_opts.default = cword
+			end
 			vim.ui.input(prompt_opts, function(input)
 				if not input or #input == 0 then
 					return
 				end
 				rename_fix_imports(input)
 			end)
+		end, bufnr)
+	else
+		assert(client.supports_method("textDocument/rename"), "Client must support textDocument/rename")
+		if new_name then
+			rename_fix_imports(new_name)
+			return
 		end
-	end
 
-	try_use_client(next(clients))
+		local prompt_opts = {
+			prompt = "New Name: ",
+			default = cword,
+		}
+		vim.ui.input(prompt_opts, function(input)
+			if not input or #input == 0 then
+				return
+			end
+			rename_fix_imports(input)
+		end)
+	end
 end
 
 return M
