9cb002b35de9ffbfd03b0fee1098d05300644f1a
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-11-24 22:19:39 +0200

    feat: working version of file rename with import fixes

--- lua/dart-lsp-refactorings/rename-file.lua
+++ lua/dart-lsp-refactorings/rename-file.lua
@@ -1,9 +1,49 @@
 local M = {}
+local rename_results
+
+local function get_dartls_client()
+	return vim.lsp.get_active_clients({ filter = "dartls" })[1]
+end
+
+local function will_rename_files(old_name, new_name, callback)
+	local params = {}
+	local dartls_client = get_dartls_client()
+	if not dartls_client then
+		return
+	end
+	local file_change = {
+		newUri = vim.uri_from_fname(new_name),
+		oldUri = vim.uri_from_fname(old_name),
+	}
+	params.files = { file_change }
+	vim.pretty_print(params)
+	dartls_client.request("workspace/willRenameFiles", params, function(err, result)
+		if err then
+			vim.pretty_print(err)
+			-- vim.notify(err.message or "Error on getting lsp rename results!")
+			return
+		end
+		callback(result)
+	end)
+end
+
 function M.before_rename(data)
-	vim.pretty_print("Before rename", data)
+	rename_results = nil
+	will_rename_files(data.source, data.destination, function(result)
+		vim.pretty_print("results!", result)
+		rename_results = result
+		data.callback()
+	end)
 end
 
 function M.after_rename(data)
-	vim.pretty_print("After rename", data)
+	vim.pretty_print("After rename", data, rename_results)
+	if rename_results ~= nil then
+		local dartls_client = get_dartls_client()
+		if not dartls_client then
+			return
+		end
+		vim.lsp.util.apply_workspace_edit(rename_results, dartls_client.offset_encoding)
+	end
 end
 return M
